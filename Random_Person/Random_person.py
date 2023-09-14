62d6c555a38d858631d786c863ce0fc3ade04c31
Pedro Maia <pedroasmaia@outlook.com>
2023-09-14 15:09:26 -0300

    Criando Pessoas ficticias com o Tkinter

--- Random_Person/Random_person.py
+++ Random_Person/Random_person.py
@@ -1,34 +1,56 @@
 import requests as r
-from tkinter import messagebox, Tk, Label, Entry, Button
+from tkinter import messagebox, Tk, Label, Entry, Button, font
+from PIL import Image, ImageTk
 import logging
 import wget
+import os
 
 
-def new_person():
+def person():
     person = r.get("https://randomuser.me/api/")
     person = person.json()
     person = person["results"]
     person = person[0]
-    picture_person = person["picture"]["medium"]
-    persona = (person["name"])
-    print(f"{person['name']}")
+    persona = person["name"]
+    name = f'{persona["first"]} {persona["last"]}'
+    gender = f'{person["gender"]}'
+    country = f'{person["location"]["country"]}'
+    picture_person = person["picture"]["large"]
+    username = person["login"]["username"]
+    password = person["login"]["password"]
+    try:
+        os.remove("./picture_person.jpeg")
+    except FileNotFoundError:
+        logging.exception("Como esperado,arquivo não existe")
     wget.download(picture_person, "./picture_person.jpeg")
-    return persona
+    return (name, gender, country, "./picture_person.jpeg",username,password)
 
-def apply_person():
-    name = new_person()
-    title_person_name = Label(text=f"Nome: {name}")
-    title_person_name.grid(column=2, row=0)
 
+def show_image(picture):
+    image = Image.open(picture)
+    photo = ImageTk.PhotoImage(image)
+    image_label = Label(image=photo)
+    image_label.image = photo
+    image_label.grid(column=1, row=2)
+def show_info(name, gender, country,username,password):
+    person_name = Label(text=f"Name: {name.title()}", font=montserrat, justify='left')
+    person_gender = Label(text=f"Gender: {gender.title()}", font=helvetica, justify='left')
+    person_country = Label(text=f"Country: {country.title()}", font=helvetica, justify='left')
+    person_username = Label(text=f"Username: {username.title()}", font=helvetica, justify='left')
+    person_password = Label(text=f"Password: {password.title()}", font=helvetica, justify='left')
+    title.grid(column=1, row=0)
+    person_name.grid(column=1, row=3)
+    person_gender.grid(column=1, row=4)
+    person_country.grid(column=1, row=5)
+    person_username.grid(column=1, row=6)
+    person_password.grid(column=1, row=7)
 if __name__ == "__main__":
-    windows = Tk()
-    logging.info("Iniciando Aplicação")
-    windows.title("Novo personagem")
-    title_person_name = Label(text="Gere um Novo Personagem")
-    title_person_name.grid(column=2, row=0)
-    new_person_button = Button(
-        text="Criar novo personagem", width=36, command=apply_person
-    )
-
-    new_person_button.grid(row=1, column=2,)
-windows.mainloop()
+    name, gender, country, picture,username,password= person()
+    window = Tk()
+    window.title("Gerando personagem")
+    helvetica = font.Font(family="Helvetica", size=16)
+    montserrat = font.Font(family="Montserrat", size=16)
+    title = Label(text="Gerador de Pessoas Fictícias", font=montserrat, justify='left')
+    show_image(picture)
+    show_info(name, gender, country,username,password)
+    window.mainloop()
